a48caf42bffba673fe8fa096efbaa67a69d51f26
KimJaeHyeok <master_catch@naver.com>
2024-05-14 10:13:41 +0900

    feat : 일상방 신규생성 / fix : 기존 코드 수정(css, js)

--- js/main.js
+++ js/main.js
@@ -13,3 +13,14 @@ function pressBtn() {
 }
 
 // 2. ERP 공부방
+
+
+// 3. search 기능
+function search() {
+    alert('아직 미구현 기능입니다 ㅠ_ㅠ');
+}
+
+// 4. 매일 반복되는 일상으로 이동하는 버튼
+function dailyRoom() {
+    location.href='../pages/dailyRoom.html';
+}
